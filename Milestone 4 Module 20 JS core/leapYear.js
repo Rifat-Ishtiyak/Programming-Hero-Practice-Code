b3cff8dcddf72a022e99c133a8b4f02fb62f671a
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2022-01-30 21:37:33 +0600

    leap year code

---
+++ Milestone 4 Module 20 JS core/leapYear.js
@@ -0,0 +1,17 @@
+
+function leapYear(year){
+    if (year % 400 == 0) {
+        console.log(year +" is a leap year.");
+     }
+     else if (year % 100 == 0) {
+        console.log(year +" is not a leap year.");
+     }
+     else if (year % 4 == 0) {
+        console.log(year +" is a leap year.");
+     }
+     else {
+        console.log(year +" is not a leap year.");
+     }
+}
+
+leapYear(1900);
