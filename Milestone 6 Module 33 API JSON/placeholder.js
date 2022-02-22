65db6e08a5cd9138c49ac00da8eb44eb40ce29c2
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2022-02-23 00:57:05 +0600

    api img & data load

--- Milestone 6 Module 33 API JSON/placeholder.js
+++ Milestone 6 Module 33 API JSON/placeholder.js
@@ -6,11 +6,31 @@ function getUser(){
 
 function showUser(data){
     for (const user of data) {
-        // console.log(`Name : ${user.name}  |  Email : ${user.email}`);
-
         const userList = document.getElementById('user-list');
         const list = document.createElement('li');
         list.innerHTML = `<b>Name</b> : ${user.name}  |  <b>Email</b> : ${user.email}`;
         userList.appendChild(list);
+    }
+}
+
+
+function getPhotos(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response =>  response.json())
+    .then(data => showPhotos(data))
+}
+
+function showPhotos(data){
+    const photoCon = document.getElementById('photo');
+    
+    for(const photo of data){
+        console.log(photo.url)
+        const photoDiv = document.createElement('div');
+        photoDiv.classList.add("images");
+        photoDiv.innerHTML=`<img src="${photo.url}" width='150' alt="">
+                            <h2>${photo.id}</h2>
+                            <h2>${photo.title}</h2>`
+        photoCon.appendChild(photoDiv);
+        photoDiv.style.border='1px solid red';
     }
 }
