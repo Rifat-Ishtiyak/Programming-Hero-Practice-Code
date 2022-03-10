446c26b0860f183430d0d130d2467b970841b300
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2022-03-10 12:05:10 +0600

    issue-tracker solved

--- Milestone 7 Module 42.5 issue-tracker/issue-tracker/main.js
+++ Milestone 7 Module 42.5 issue-tracker/issue-tracker/main.js
@@ -23,15 +23,24 @@ function submitIssue(e) {
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => parseInt(issue.id) === id);
   currentIssue.status = 'Closed';
+
+  
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
+  
+  if(issues.find(issue => parseInt(issue.id) === id)){
+    const ob = document.getElementById(`${id}`);
+    ob.style.textDecoration ='line-through';
+    console.log(ob);
+  }
 }
 
 const deleteIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter( issue => parseInt(issue.id) !== id );
+  console.log(remainingIssues);
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
 }
 
@@ -46,10 +55,10 @@ const fetchIssues = () => {
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
+                              <h3 id="${id}"> ${description} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
