c6cf5cc2ab32f2782b94d113df5ea266702c552a
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2022-03-17 17:41:27 +0600

    error fixed for empty Object

--- Milestone 8  Module 46 rest-countries-react/rest-countries-api/src/Components/Country/Country.js
+++ Milestone 8  Module 46 rest-countries-react/rest-countries-api/src/Components/Country/Country.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
-    const {flags ,name, capital, population, region, area} = props.country;
-    // console.log(borders);
+    let {flags ,name, capital, population, region, area, languages} = props.country;
+
+    if(languages === null || languages === undefined){
+        languages ={value: "kisu nai"};
+    }
+    
     return (
         <div className='country'>
             <img src={flags.png} alt="" width={100}/>
@@ -13,7 +17,7 @@ const Country = (props) => {
                 Population : {population} <br />
                 Region : {region} <br />
                 Area : {area} <br />
-                {/* borders : {borders + ","} */}
+                Languages : {Object.values(languages)}
             </p>
         </div>
     );
