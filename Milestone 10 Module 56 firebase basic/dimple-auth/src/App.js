b50f86891c4051be4ae6003a2968f40b4fff6372
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2022-04-06 18:04:16 +0600

    dimple-practice added

---
+++ Milestone 10 Module 56 firebase basic/dimple-auth/src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="px-24">
+      <h1 className="text-4xl text-center text-blue-700 font-bold font-mono my-5">
+        FireBase Authentication Practice
+      </h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et placeat
+        atque perspiciatis harum. Corrupti, incidunt qui dolores delectus enim
+        quas deleniti asperiores reiciendis. Ea est dicta, magni illum quibusdam
+        ratione dolores vel repellat obcaecati, accusamus nesciunt. Quam,
+        officia. Iusto harum porro, obcaecati modi nobis ipsa libero,
+        perspiciatis ad molestiae, doloremque placeat magnam praesentium! Illum
+        obcaecati numquam est neque. Facilis error iure repudiandae a impedit
+        officia exercitationem cumque fugiat repellendus maiores ducimus
+        voluptas rerum labore assumenda ea distinctio consequatur natus, modi
+        porro illo sequi nostrum alias. Aliquam excepturi unde impedit nihil
+        harum. Maxime nulla, sequi eligendi possimus laudantium similique rem
+        nisi est iste unde asperiores vitae. Ducimus velit accusamus adipisci
+        suscipit corporis sunt quaerat eveniet sit perferendis minima. Animi
+        repudiandae eos modi quia hic, doloribus facilis a dolores recusandae
+        aut quam laborum ab totam, ullam deserunt incidunt quos distinctio illo
+        nostrum facere ut consequatur aspernatur. Mollitia unde distinctio
+        explicabo optio dicta aliquid laudantium molestias animi. Nisi assumenda
+        asperiores commodi expedita quis, sed quidem dolore blanditiis, nostrum
+        impedit praesentium officiis. Unde nisi adipisci, aspernatur officia
+        voluptate, velit beatae laborum laudantium ipsa optio id aliquam debitis
+        rerum! Eius voluptas voluptate rem? Similique culpa nam repudiandae
+        totam rerum maxime ipsam minima ducimus voluptatem nisi.
+      </p>
+    </div>
+  );
+}
+
+export default App;
